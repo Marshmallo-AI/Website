dcd450cb10e4903349bf5b21f00840a9db29903c
Aman Jaglan <aman@arc.computer>
2026-01-15 14:49:38 -0500

    removed the beta string

--- app/page.tsx
+++ app/page.tsx
@@ -350,10 +350,6 @@ export default function HomePage() {
                     </Button>
                   </Link>
                 </Flex>
-
-                <Text className="hero-beta-notice">
-                  Marlo is currently in beta. For early access, <Link href="#contact" className="hero-beta-link">reach out</Link>
-                </Text>
               </div>
             </div>
           </div>
